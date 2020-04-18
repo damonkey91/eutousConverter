795173fb040db76164d6a89a83f0cfc7dc7876b9
Tobias Johansson <tobiasjohansson911@gmail.com>
2020-04-18 22:17:25 +0200

    added splash screen

--- app/src/main/java/com/example/mrx/exchangeandusatoeuconverter/ActivitiesAndFragments/Exchange.java
+++ app/src/main/java/com/example/mrx/exchangeandusatoeuconverter/ActivitiesAndFragments/Exchange.java
@@ -28,6 +28,7 @@ public class Exchange extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange);
         toolbar = findViewById(R.id.toolbar);
